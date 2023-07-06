c87cce53e3c4d58e768866117d17362ff2788fa9
J Neal <jasmineneal101@gmail.com>
2023-07-07 03:59:18 +1000

    push updates

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,33 +1,31 @@
 import React from "react";
 
 function navigation(props) {
-    const tabs = ["About", "Portfolio", "Contact"];
-    return (
-      <div className="tabs is-centered">
-        <ul className="nav nav-tabs">
-          {tabs.map((tab) => (
-            <li
+  const tabs = ["About", "Portfolio", "Contact"];
+  return (
+    <div className="tabs is-centered">
+      <ul className="nav nav-tabs">
+        {tabs.map((tab) => (
+          <li
+            className={
+              props.currentPage === tab ? "nav-item is-active" : "nav-item"
+            }
+            key={tab}
+          >
+            <a
+              href={"#" + tab.toLowerCase()}
+              onClick={() => props.handlePageChange(tab)}
               className={
-                props.currentPage === tab ? "nav-item is-active" : "nav-item"
+                props.currentPage === tab ? "nav-link active" : "nav-link"
               }
-              key={tab}
             >
-              <a
-                href={"#" + tab.toLowerCase()}
-                // Whenever a tab is clicked on,
-                // the current page is set through the handlePageChange props.
-                onClick={() => props.handlePageChange(tab)}
-                className={
-                  props.currentPage === tab ? "nav-link active" : "nav-link"
-                }
-              >
-                {tab}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-  
-  export default navigation;
+              {tab}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default navigation;
