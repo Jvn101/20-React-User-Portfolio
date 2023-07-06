fdda743aa93fdbbc08bc89336e76e6667d7cb231
J Neal <jasmineneal101@gmail.com>
2023-07-07 05:03:36 +1000

    resolve image erro

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,42 +6,42 @@ function Portfolio(){
 const [portfolio] = useState([
   {
     title: "Password Generator",
-    image: "/Images/passwordGenerator.png",
+    image: "/public/Images/passwordGenerator.png",
     description: "Website showcasing a random password generator that outputs passwords on user input.",
     repository: "https://github.com/Jvn101/03-JavaScript-Password-Generator",
     deployedlink: "https://jvn101.github.io/03-JavaScript-Password-Generator/"
   },
   {
     title: "Web APIs Code Quiz",
-    image: "/Images/webAPIQuiz.png",
+    image: "/public/Images/webAPIQuiz.png",
     description: "Timed quiz using JavaScript that stores high scores.",
     repository: "https://github.com/Jvn101/04-Web-APIs-Code-Quiz",
     deployedlink: "https://jvn101.github.io/04-Web-APIs-Code-Quiz/"
   },
   {
     title: "API Weather Dashboard",
-    image: "/Images/weatherAPI.png",
+    image: "/public/Images/weatherAPI.png",
     description: "Weather dashboard that features todays weather followed by a five day forecast.",
     repository: "https://github.com/Jvn101/06-Server-Side-APIs-Weather-Dashboard",
     deployedlink: "https://jvn101.github.io/06-Server-Side-APIs-Weather-Dashboard/"
   },
   {
     title: "Express.js Note Taker",
-    image: "Images/noteTaker.png",
+    image: "/public/Images/noteTaker.png",
     description: "This application is called Note Taker and can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file. The project was created with code from edX Boot Camps.",
     repository: "https://github.com/Jvn101/11-Express.js-Note-Taker",
     deployedlink: "https://notetakerapplication-week11.herokuapp.com/"
   },
   {
     title: "MVC Tech Blog",
-    image: "/Images/TheTechBlog.png",
+    image: "/public/Images/TheTechBlog.png",
     description: "This application is a CMS-style blog site (similar to a Wordpress site), where developers can publish their blog posts and comment on other developers posts as well.",
     repository: "https://github.com/Jvn101/14-MVC-Tech-Blog",
     deployedlink: ""
   },
   {
     title: "NoSQL Social Network",
-    image: "/Images/NoSQLSocialNetwork.png",
+    image: "/public/Images/NoSQLSocialNetwork.png",
     description: "This application ia an API for a social network web page where users can share their thoughts, react to friends’ thoughts, and create a friend list.",
     repository: "https://github.com/Jvn101/18-NoSQL-Social-Network-API",
     deployedlink: ""
